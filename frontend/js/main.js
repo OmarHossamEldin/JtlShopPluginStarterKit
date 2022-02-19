43dc0491ff969bda8bdda3c4491574b32ba4bb9f
omarhossameldin <okandil273@gmail.com>
2022-02-19 12:29:39 +0200

    loading-more-assets-and-enhancment-for-request-function-using-oop-approach

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,27 +1,3 @@
-// start send request [access rescource from backend]
-async function sendingRequest(url, method, data) {
-  const response = await fetch(
-    `${url}?attributeName=${
-      data.productName
-    }&language=${searchForm.getAttribute("language")}`,
-    {
-      method: method,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
-
-  return {
-    status: response.status,
-    data: await response.json(),
-  };
-}
-// end send request [access rescource from backend]
-
-//
-const protocol = window.protocol,
-      hostname = window.hostname,
-      basUrl = `${protocol}://${hostname}/point_router`;
-
-sendingRequest(basUrl, 'GET', 'test').then(response => console.log(response));
+const basUrl = `${location.protocol}://${location.host}`,
+      httpRequest = new HttpRequest(basUrl);
+      httpRequest.get('/point_router');
