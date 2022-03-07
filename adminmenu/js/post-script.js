1ef7b9c2a395dc3da47e7b5059bd29e042bab2cb
dev-fathy <mohamedfathy573@gmail.com>
2022-03-07 15:52:29 +0200

    modify javascript request

--- adminmenu/js/post-script.js
+++ adminmenu/js/post-script.js
@@ -1,16 +1,39 @@
 let updatePostEvent = document.querySelectorAll(".update-this-item");
 
-let postId = document
-.querySelector(".update-this-item")
-.getAttribute("date-attributes");
 
-updatePostEvent.forEach((e)=>{
+updatePostEvent.forEach((e) => {
 
     e.addEventListener("click", (e) => {
-    
-        console.log('hi');
-    
-        console.log(postId);
-    
+        e.preventDefault();
+
+        let postId = e.target.getAttribute("date-attributes");
+
+        let pluginUrl = e.target.getAttribute("plugin-url");
+
+        let token = document.querySelector('.jtl_token').value;
+
+        let sentData = { "post_id": postId };
+
+        let headers = {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            jtlToken: token,
+        };
+
+        let url = pluginUrl + 'ressource';
+        const basUrl = url + '?fetch=fathy',
+            httpRequest = new HttpRequest(basUrl, headers);
+
+        const postData = httpRequest.post(basUrl, sentData);
+
+        postData.then((data) => {
+            let post = data.data.post;
+
+            document.getElementById("posteditmodal").click();
+            document.getElementById("postId").value = post.id;
+            document.getElementById("post-title").value = post.title;
+            document.getElementById("post-body").textContent = post.body;
+        });
+
     })
 })
