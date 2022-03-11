ffedfe4cd0c4f813f478a26d30f8f698119898d0
dev-fathy <mohamedfathy573@mail.com>
2022-03-11 15:15:17 +0200

    add-payment-models

---
+++ Src/Models/ApiCredentials.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Plugin\JtlShopPluginStarterKit\Src\Models;
+
+use Plugin\JtlShopPluginStarterKit\Src\Database\Initialization\Model;
+
+class ApiCredentials extends Model
+{
+    protected $table    = 'tec_see_api_credentials';
+
+    protected $primaryKey  = 'id';
+
+    protected $fillable = [
+        'business_account',
+        'client_id',
+        'secret_key'
+    ];
+}
