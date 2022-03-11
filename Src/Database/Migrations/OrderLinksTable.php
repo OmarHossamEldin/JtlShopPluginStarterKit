9428aeb65c8433064087a25e9de0884ed945c089
dev-fathy <mohamedfathy573@mail.com>
2022-03-11 15:14:45 +0200

    add-payment-tables

---
+++ Src/Database/Migrations/OrderLinksTable.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Plugin\JtlShopPluginStarterKit\Src\Database\Migrations;
+
+use Plugin\JtlShopPluginStarterKit\Src\Database\Initialization\Schema;
+use Plugin\JtlShopPluginStarterKit\Src\Database\Initialization\Table;
+
+class OrderLinksTable
+{
+    public function up()
+    {
+        Schema::create('tec_see_order_links', function (Table $table) {
+            $table->id();
+            $table->string('order_id');
+            $table->string('order_status');
+            $table->string('order_link');
+            $table->string('link_name');
+            $table->string('order_method');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('tec_see_order_links');
+    }
+}
