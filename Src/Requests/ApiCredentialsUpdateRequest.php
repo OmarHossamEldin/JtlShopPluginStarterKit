3aa8795ba9c4ec962bf145062c129a782c24dfc7
dev-fathy <mohamedfathy573@mail.com>
2022-03-11 15:16:34 +0200

    create-new-form-request

---
+++ Src/Requests/ApiCredentialsUpdateRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Plugin\JtlShopPluginStarterKit\Src\Requests;
+
+use Plugin\JtlShopPluginStarterKit\Src\Traits\ValidationTrait;
+use Plugin\JtlShopPluginStarterKit\Src\Support\Http\Request;
+
+class ApiCredentialsUpdateRequest extends Request
+{
+    use ValidationTrait;
+
+    public function rules()
+    {
+        return [
+            'business_account' => 'required',
+            'client_id' => 'required',
+            'secret_key' => 'required',
+        ];
+    }
+}
