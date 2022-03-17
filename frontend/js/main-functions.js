f97eb52496a299b91b16a1fcb32fc80b8a38b4a5
ahmed-b20 <ahmedbanawiwork@gmail.com>
2022-03-17 11:49:16 +0200

    make function work with all type of selector

--- frontend/js/main-functions.js
+++ frontend/js/main-functions.js
@@ -1,18 +1,59 @@
 // start build element
-function elementBuilder(elementName, elementAttribute, attributeNumber, elementContent, contentType, elementNumber, parentElement) {
-    for (let index = 0; index < elementNumber; index++) {
-        let childElement = document.createElement(elementName)
+function elementBuilder(
+  elementName,
+  elementAttribute,
+  contentType,
+  elementContent,
+  elementNumber,
+  parentElement
+) {
+  // start main function to build element
+  function mainWork(childElement) {
+    for (let index = 0; index < elementAttribute.length; index++) {
+      let objectKeys = Object.keys(elementAttribute[index]);
+      let objectValues = Object.values(elementAttribute[index]);
 
-        for (let index = 0; index < attributeNumber; index++) {
-            childElement.setAttribute(elementAttribute[index].name, elementAttribute[index].value)
-        }
-
-        childElement.contentType = elementContent
-        
-        if (!!parentElement) {
-            appendElement(parentElement, childElement)
-        }
+      for (let keyNumber = 0; keyNumber < objectKeys.length; keyNumber++) {
+        childElement.setAttribute(
+          objectKeys[keyNumber],
+          objectValues[keyNumber]
+        );
+      }
     }
+
+    if (contentType === "innerHTML") {
+      childElement.innerHTML = elementContent;
+    } else if (contentType === "value") {
+      childElement.value = elementContent;
+    } else if (contentType === "textContent") {
+      childElement.textContent = elementContent;
+    }
+  }
+  // end main function to build element
+
+  if (parentElement.length > 1) {
+    parentElement.forEach((e) => {
+      for (let index = 0; index < elementNumber; index++) {
+        let childElement = document.createElement(elementName);
+
+        mainWork(childElement);
+
+        if (!!parentElement) {
+          appendElement(e, childElement);
+        }
+      }
+    });
+  } else {
+    for (let index = 0; index < elementNumber; index++) {
+      let childElement = document.createElement(elementName);
+
+      mainWork(childElement);
+
+      if (!!parentElement) {
+        appendElement(parentElement, childElement);
+      }
+    }
+  }
 }
 // end build element
 
@@ -20,24 +61,24 @@ function elementBuilder(elementName, elementAttribute, attributeNumber, elementC
 
 // start append function
 function appendElement(parentElement, childElement) {
-    parentElement.appendChild(childElement)
+  parentElement.appendChild(childElement);
 }
 // end append function
 
 // ========================================
 
-// start request validation 
+// start request validation
 function requestValidation(response, needFunction) {
-    if (response.status === 201) {
-        needFunction()
-    } else if (response.status === 200) {
-        needFunction()
-    } else if (response.status === 422) {
-        errorFunction()
-    } else if (response.status === 403) {
-        errorFunction()
-    } else {
-        errorFunction()
-    }
+  if (response.status === 201) {
+    needFunction();
+  } else if (response.status === 200) {
+    needFunction();
+  } else if (response.status === 422) {
+    errorFunction();
+  } else if (response.status === 403) {
+    errorFunction();
+  } else {
+    errorFunction();
+  }
 }
 // end work in validation
