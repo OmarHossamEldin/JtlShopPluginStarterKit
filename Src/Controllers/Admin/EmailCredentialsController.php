b5246dfe88b07c3afb1f6cbbc370c72b7ae78eb9
dev-fathy <mohamedfathy573@mail.com>
2022-04-11 14:56:54 +0200

    modify-controller-functions

--- Src/Controllers/Admin/EmailCredentialsController.php
+++ Src/Controllers/Admin/EmailCredentialsController.php
@@ -8,6 +8,10 @@ use Plugin\JtlShopPluginStarterKit\Src\Models\EmailCredential;
 use Plugin\JtlShopPluginStarterKit\Src\Requests\EmailCredentialStoreRequest;
 use Plugin\JtlShopPluginStarterKit\Src\Requests\EmailCredentialUpdateRequest;
 use JTL\Shop;
+use Plugin\JtlShopPluginStarterKit\Src\Database\Migrations\EmailCredentialsTable;
+use Plugin\JtlShopPluginStarterKit\Src\Middlewares\CheckApiCredentials;
+use Plugin\JtlShopPluginStarterKit\Src\Requests\deleteEmailCredentialsRequest;
+use Plugin\JtlShopPluginStarterKit\Src\Requests\getEmailCredentialsRequest;
 use Plugin\JtlShopPluginStarterKit\Src\Validations\Alerts;
 use Plugin\JtlShopPluginStarterKit\Src\Support\Debug\Debugger;
 
@@ -18,7 +22,7 @@ class EmailCredentialsController
     {
         $smarty   = Shop::Smarty();
         $emailCredential     = new EmailCredential();
-        $emailCredentials    = $emailCredential->select('id', 'email','mail_host', 'username', 'password', 'port')->get();
+        $emailCredentials    = $emailCredential->select('id', 'email', 'mail_host', 'username', 'password', 'port')->get();
         $smarty->assign('emailCredentials', $emailCredentials);
     }
 
@@ -45,25 +49,62 @@ class EmailCredentialsController
         ]);
 
         return Response::json(["message" => "Record is created successfully"], 201);
-
     }
 
-/*     public function update(EMailCredentialUpdateRequest $request)
+    /**
+     * update a post
+     *
+     * @param EMailCredentialUpdateRequest $request
+     * @param integer $pluginId
+     * @return void
+     */
+    public function update(EMailCredentialUpdateRequest $request)
+    {
+        $checkCredentials = new CheckApiCredentials;
+        $checkCredentials->handle();
+
+        $validatedData = $request->validated();
+        $post = new EmailCredential();
+        $post->update([
+            'email' => $validatedData['email'],
+            'mail_host' => $validatedData['mail_host'],
+            'username' => $validatedData['username'],
+            'password' => $validatedData['password'],
+            'port' => $validatedData['port'],
+        ], $validatedData['emailCredentialId']);
+        return Response::json([
+            'message' => 'email credentials are updated successfully',
+        ], 201);
+    }
+
+    
+    public function destroy(deleteEmailCredentialsRequest $request)
     {
         $validatedData = $request->validated();
-        $params = $request->get_route_params();
         $emailCredential = new EmailCredential();
-        $emailCredential->update($validatedData, 'id');
-        $emailCredential = $emailCredential->first($params['id']);
-        return Response::json(['message' => 'Mail Credentials Update Successfully.', 'mail_host' => $emailCredential], 206);
+        $emailCredential->delete($validatedData['emailCredentialId']);
+        return Response::json(['message' => 'Mail Credentials Deleted Successfully.'], 204);
     }
 
-    public function destroy(Request $request)
+    /**
+     * get email data
+     *
+     * @param getCredentialRequest $request
+     * @param integer $pluginId
+     * @return void
+     */
+    public function getEmailCredentials(getEmailCredentialsRequest $request)
     {
-        $params = $request->get_route_params();
-        $emailCredential = new EmailCredential();
-        $emailCredential->delete('id');
-        $emailCredential = $emailCredential->first($params['id']);
-        return Response::json(['message' => 'Mail Credentials Deleted Successfully.'], 204);
-    } */
+        $validatedData = $request->validated();
+        $credential = new EmailCredential();
+        $credentialData = $credential->select('id', 'email', 'mail_host', 'username', 'password', 'port')
+            ->where('id', $validatedData['emailCredentialId'])
+            ->get();
+
+        $emailCredential = (object)$credentialData[0];
+
+        return Response::json([
+            'emailCredential' => $emailCredential,
+        ], 200);
+    }
 }
--- Src/Controllers/Admin/PostController.php
+++ Src/Controllers/Admin/PostController.php
@@ -10,6 +10,7 @@ use Plugin\JtlShopPluginStarterKit\Src\Helpers\Response;
 use Plugin\JtlShopPluginStarterKit\Src\Middlewares\CheckApiCredentials;
 use Plugin\JtlShopPluginStarterKit\Src\Requests\getPostDetailsRequest;
 use Plugin\JtlShopPluginStarterKit\Src\Requests\PostDeleteRequest;
+use Plugin\JtlShopPluginStarterKit\Src\Requests\PostUpdateRequest;
 use Plugin\JtlShopPluginStarterKit\Src\Support\Debug\Debugger;
 use Plugin\JtlShopPluginStarterKit\Src\Support\Http\Session;
 use Plugin\JtlShopPluginStarterKit\Src\Validations\Alerts;
@@ -29,7 +30,7 @@ class PostController
         $post     = new Post();
 
 
-        $posts    = $post->select('id','title', 'body')
+        $posts    = $post->select('id', 'title', 'body')
             ->with('category:name,description')->get();
         $smarty->assign('posts', $posts);
     }
@@ -48,8 +49,39 @@ class PostController
 
         $validatedData = $request->validated();
         $post = new Post();
-        $post->create($validatedData);
-        Alerts::show('success', ['post' => 'is created successfully']);
+        $post->create([
+            'title' => $validatedData['title'],
+            'body' => $validatedData['body'],
+            'tec_see_category_id' => $validatedData['tec_see_category_id'],
+        ]);
+
+        return Response::json([
+            'message' => 'post is created successfully',
+        ], 201);
+    }
+
+    /**
+     * update a post
+     *
+     * @param PostUpdateRequest $request
+     * @param integer $pluginId
+     * @return void
+     */
+    public function update(PostUpdateRequest $request)
+    {
+        $checkCredentials = new CheckApiCredentials;
+        $checkCredentials->handle();
+
+        $validatedData = $request->validated();
+        $post = new Post();
+        $post->update([
+            'title' => $validatedData['title'],
+            'body' => $validatedData['body'],
+            'tec_see_category_id' => $validatedData['tec_see_category_id'],
+        ], $validatedData['postId']);
+        return Response::json([
+            'message' => 'post is updated successfully',
+        ], 201);
     }
 
     /**
@@ -64,7 +96,9 @@ class PostController
         $validatedData = $request->validated();
         $post = new Post();
         $post->delete($validatedData['postId']);
-        Alerts::show('success', ['post' => 'is deleted successfully']);
+        return Response::json([
+            'message' => 'post is deleted successfully',
+        ], 201);
     }
 
     /**
@@ -78,8 +112,8 @@ class PostController
     {
         $validatedData = $request->validated();
         $post = new Post();
-        $postData = $post->select('id', 'title', 'body')
-            ->where('id', $validatedData['post_id'])
+        $postData = $post->select('id', 'title', 'body', 'tec_see_category_id')
+            ->where('id', $validatedData['postId'])
             ->get();
 
         $post = (object)$postData[0];
