2660ea9cc63c82ebf0239e0d159058db9bb8c3bc
dev-fathy <mohamedfathy573@mail.com>
2022-04-11 14:57:31 +0200

    modify-routes

--- Src/Services/RoutesService.php
+++ Src/Services/RoutesService.php
@@ -12,19 +12,13 @@ class RoutesService
     {
         Route::execute('Admin\ApiCredentialsController@index');
         Route::execute('Admin\PostController@index');
+        Route::execute('Admin\CategoryController@index');
         Route::execute('Admin\EmailCredentialsController@index');
+        
     }
 
     public function backend_endpoints()
     {
-        Route::post('posts', 'Admin\PostController@store');
-        Route::delete('posts', 'Admin\PostController@destroy');
-
-
-        Route::post('api-credentials', 'Admin\ApiCredentialsController@create');
-        Route::delete('api-credentials', 'Admin\ApiCredentialsController@destroy');
-        Route::put('api-credentials', 'Admin\ApiCredentialsController@update');
-
         Route::resolveApi(Request::uri(), Request::type());
     }
 
@@ -39,10 +33,27 @@ class RoutesService
 
         Route::post('/store/email-credentials', 'Admin\EmailCredentialsController@store');
         Route::post('/test/email', 'Admin\TestEmailCredentialsController@testEmailCredentials');
+        Route::post('/get/email-credential', 'Admin\EmailCredentialsController@getEmailCredentials');
+        Route::post('/update/email-credential', 'Admin\EmailCredentialsController@update');
+        Route::post('/delete/email-credential', 'Admin\EmailCredentialsController@destroy');
 
 
-        Route::post('/get-post/{id}', 'Admin\PostController@getPostData');
-        Route::post('/get-credential/{id}', 'Admin\ApiCredentialsController@getCredentialData');
+        Route::post('/store/post', 'Admin\PostController@store');
+        Route::post('/update/post', 'Admin\PostController@update');
+        Route::post('/get/post', 'Admin\PostController@getPostData');
+        Route::post('/delete/post', 'Admin\PostController@destroy');
+
+
+        Route::post('/store/category', 'Admin\CategoryController@store');
+        Route::post('/update/category', 'Admin\CategoryController@update');
+        Route::post('/get/category', 'Admin\CategoryController@getPostData');
+        Route::post('/delete/category', 'Admin\CategoryController@destroy');
+
+
+        Route::post('/store/api-credential', 'Admin\ApiCredentialsController@store');
+        Route::post('/update/api-credential', 'Admin\ApiCredentialsController@update');
+        Route::post('/get/api-credential', 'Admin\ApiCredentialsController@getCredentialData');
+        Route::post('/delete/api-credential', 'Admin\ApiCredentialsController@destroy');
 
         Route::resolveApi(Request::uri(), Request::type());
     }
