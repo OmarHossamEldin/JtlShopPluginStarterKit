60f61562425b896f7a7e40238882a0f1f45946c1
dev-fathy <mohamedfathy573@mail.com>
2022-04-15 16:09:00 +0200

    modify-routes

--- Src/Services/RoutesService.php
+++ Src/Services/RoutesService.php
@@ -2,6 +2,9 @@
 
 namespace Plugin\JtlShopPluginStarterKit\Src\Services;
 
+use Plugin\JtlShopPluginStarterKit\Src\Exceptions\DatabaseQueryException;
+use Plugin\JtlShopPluginStarterKit\Src\Exceptions\RouteNotFoundException;
+use Plugin\JtlShopPluginStarterKit\Src\Helpers\Response;
 use Plugin\JtlShopPluginStarterKit\Src\Support\Facades\Router\Route;
 use Plugin\JtlShopPluginStarterKit\Src\Support\Http\Request;
 
@@ -10,39 +13,43 @@ class RoutesService
 
     public function backend_executions()
     {
-        Route::execute('Admin\ApiCredentialsController@index');
-        Route::execute('Admin\PostController@index');
-        Route::execute('Admin\CategoryController@index');
-        Route::execute('Admin\EmailCredentialsController@index');
-        
     }
 
     public function backend_endpoints()
     {
-        Route::post('/get/categories', 'Admin\CategoryController@index');
-        Route::post('/categories', 'Admin\CategoryController@store');
-        Route::post('/update/categories/{id}', 'Admin\CategoryController@update');
-        Route::post('/delete/categories/{id}', 'Admin\CategoryController@destroy');
+        MiddlewaresService::load();
 
-        Route::post('/get/posts', 'Admin\PostController@index');
-        Route::post('/posts', 'Admin\PostController@store');
-        Route::post('/update/posts/{id}', 'Admin\PostController@update');
-        Route::post('/delete/posts/{id}', 'Admin\PostController@destroy');
+        Route::group(['CheckApiCredentials'], function () {
 
-        Route::post('/get/api-credentials', 'Admin\ApiCredentialsController@index');
-        Route::post('/api-credentials', 'Admin\ApiCredentialsController@store');
-        Route::post('/update/api-credentials/{id}', 'Admin\ApiCredentialsController@update');
-        Route::post('/delete/api-credentials/{id}', 'Admin\ApiCredentialsController@destroy');
+            Route::post('/get/categories', 'Admin\CategoryController@index');
+            Route::post('/categories', 'Admin\CategoryController@store');
+            Route::post('/update/categories/{id}', 'Admin\CategoryController@update');
+            Route::post('/delete/categories/{id}', 'Admin\CategoryController@destroy');
 
-        Route::post('/get/email-credentials', 'Admin\EmailCredentialsController@index');
-        Route::post('/email-credentials', 'Admin\EmailCredentialsController@store');
-        Route::post('/update/email-credentials/{id}', 'Admin\EmailCredentialsController@update');
-        Route::post('/delete/email-credentials/{id}', 'Admin\EmailCredentialsController@destroy');
+            Route::post('/get/posts', 'Admin\PostController@index');
+            Route::post('/posts', 'Admin\PostController@store');
+            Route::post('/update/posts/{id}', 'Admin\PostController@update');
+            Route::post('/delete/posts/{id}', 'Admin\PostController@destroy');
 
-        Route::post('/test/email', 'Admin\TestEmailCredentialsController@testEmailCredentials');
+            Route::post('/get/api-credentials', 'Admin\ApiCredentialsController@index');
+            Route::post('/api-credentials', 'Admin\ApiCredentialsController@store');
+            Route::post('/update/api-credentials/{id}', 'Admin\ApiCredentialsController@update');
+            Route::post('/delete/api-credentials/{id}', 'Admin\ApiCredentialsController@destroy');
 
+            Route::post('/get/email-credentials', 'Admin\EmailCredentialsController@index');
+            Route::post('/email-credentials', 'Admin\EmailCredentialsController@store');
+            Route::post('/update/email-credentials/{id}', 'Admin\EmailCredentialsController@update');
+            Route::post('/delete/email-credentials/{id}', 'Admin\EmailCredentialsController@destroy');
 
-        Route::resolveApi(Request::uri(), Request::type());
+            Route::post('/test/email', 'Admin\TestEmailCredentialsController@testEmailCredentials');
+        });
+        try {
+            Route::resolveApi(Request::uri(), Request::type());
+        } catch (RouteNotFoundException $exception) {
+            return Response::json(['message' => $exception->getMessage()], 404);
+        } catch (DatabaseQueryException $exception) {
+            return Response::json(['message' => $exception->getMessage()], 422);
+        }
     }
 
     public function frontend_executions()
