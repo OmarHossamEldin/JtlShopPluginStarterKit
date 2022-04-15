fcf1a9d12f11812f4238e5598e15a356e5577da0
dev-fathy <mohamedfathy573@mail.com>
2022-04-15 16:33:41 +0200

    modify-credentials

--- Src/Middlewares/CheckApiCredentials.php
+++ Src/Middlewares/CheckApiCredentials.php
@@ -5,11 +5,17 @@ namespace Plugin\JtlShopPluginStarterKit\Src\Middlewares;
 use Plugin\JtlShopPluginStarterKit\Src\Helpers\Response;
 use Plugin\JtlShopPluginStarterKit\Src\Support\Facades\Middleware\BaseMiddleware;
 use Plugin\JtlShopPluginStarterKit\Src\Models\ApiCredentials;
+use Plugin\JtlShopPluginStarterKit\Src\Support\Debug\Debugger;
+use Plugin\JtlShopPluginStarterKit\Src\Support\Http\Request;
 
 class CheckApiCredentials
 {
     public static function handle()
     {
+        $request = new Request;
+        $data = $request->all();
+
+        if(!isset($data['page'])){
             $credential     = new ApiCredentials;
 
             $searchForCredentials    = $credential->select('client_id')->get();
@@ -19,6 +25,7 @@ class CheckApiCredentials
                     'message' => 'Note: You have to store Api credentials first.',
                 ], 422);
             }
+        }
         
     }
 }
