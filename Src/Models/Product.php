376d36d1cc0402728ff77ed54248ec60f5ea8e03
dev-fathy <mohamedfathy573@gmail.com>
2022-05-18 17:00:58 +0200

    add-new-model

---
+++ Src/Models/Product.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Plugin\JtlShopPluginStarterKit\Src\Models;
+
+use Plugin\JtlShopPluginStarterKit\Src\Database\Orm\Model;
+
+class Product extends Model
+{
+    protected $table    = 'tartikel';
+
+    protected $primaryKey  = 'kArtikel';
+
+    protected $fillable = [
+
+        'kHersteller', // kManufacturer
+        'kLieferstatus', // kDelivery status
+        'kSteuerklasse', // kTax class
+        'kEinheit', // kUnit
+        'kVersandklasse', // kShipping class
+        'kEigenschaftKombi', // kPropertyCombination
+        'kVaterArtikel', // kFatherArticle
+        'kStueckliste', // kStock list
+        'kWarengruppe', // kWare group
+        'kVPEEinheit', // kVPE unit
+        'kMassEinheit', // kMeasurement unit
+        'kGrundPreisEinheit', // kPriceUnit
+        'cSeo', // cSeo
+        'cArtNr', // cArtNo
+        'cName', // cName
+        'cBeschreibung', // cDescription
+        'cAnmerkung', // cNote
+        'fLagerbestand', // fStock
+        'fStandardpreisNetto', // fStandard priceNetto
+        'fMwSt', // fMwSt
+        'fMindestbestellmenge', // fMinimum order quantity
+        'fLieferantenlagerbestand', // fSupplier inventory
+        'fLieferzeit', // fDelivery time
+        'cBarcode', // cBarcode
+        'cTopArtikel', // cTopArticle
+        'fGewicht', // fWeight
+        'fArtikelgewicht', // fArticle weight
+        'fMassMenge', // fMassQuantity
+        'fGrundpreisMenge', // fPriceQuantity
+        'fBreite', // fWidth
+        'fHoehe', // fHeight
+        'fLaenge', // fLength
+        'cNeu', // cNew
+        'cKurzBeschreibung', // cShortDescription
+        'fUVP', // fUVP
+        'cLagerBeachten', // cBearing Note
+        'cLagerKleinerNull', // cCamp SmallNull
+        'cLagerVariation', // cBearing variation
+        'cTeilbar', // cDivisible
+        'fPackeinheit', // fPacking unit
+        'fAbnahmeintervall', // fAcceptance interval
+        'fZulauf', // fInflow
+        'cVPE', // cVPE
+        'fVPEWert', // fVPEValue
+        'cVPEEinheit', // cVee unit
+        'cSuchbegriffe', // cSearch terms
+        'nSort', // nSort
+        'dErscheinungsdatum', // dEdition date
+        'dErstellt', // dCreated
+        'dLetzteAktualisierung', // dLastUpdate
+        'dZulaufDatum', // dDeliveryDate
+        'dMHD', // dMHD
+        'cSerie', // cSeries
+        'cISBN', // cISBN
+        'cASIN', // cASIN
+        'cHAN', // cHAN
+        'cUNNummer', // cUNNumber
+        'cGefahrnr', // cDangernr
+        'cTaric', // cTaric
+        'cUPC', // cUPC
+        'cHerkunftsland', // cCountry of origin
+        'cEPID', // cEPID
+        'nIstVater', // nIstFather
+        'nLiefertageWennAusverkauft', // nDeliveryDaysWhenSoldOut
+        'nAutomatischeLiefertageberechnung', // nAutomaticDeliveryDateCalculationOut
+        'nBearbeitungszeit' // nProcessing time
+
+    ];
+}
