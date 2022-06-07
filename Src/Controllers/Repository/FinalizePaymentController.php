4d07f73d7c7cdf039b825603bd6c118a3b7031bb
dev-fathy <mohamedfathy573@gmail.com>
2022-06-07 10:22:35 +0200

    modify-repository-files

--- Src/Controllers/Repository/FinalizePaymentController.php
+++ Src/Controllers/Repository/FinalizePaymentController.php
@@ -15,6 +15,12 @@ use MvcCore\Jtl\Models\OrderLink;
 
 class FinalizePaymentController
 {
+        private TokenValidatorRepository $tokenValidatorRepository;
+
+        public function __construct()
+        {
+                $this->tokenValidatorRepository = new TokenValidatorRepository();
+        }
 
         public function confirm_payment(Request $request, $smarty)
         {
@@ -34,11 +40,8 @@ class FinalizePaymentController
                         if ($link['link_name'] === 'self') {
                                 $orderUrl = $link['order_link'];
                                 // send request 
-                                $checkToken = new CheckTokenExpiration;
-                                $results = $checkToken->check();
 
-                                $tokenName = $results['tokenName'];
-                                $tokenType = $results['tokenType'];
+                                [$tokenName, $tokenType] = $this->tokenValidatorRepository->get_token_data();
 
 
                                 $curl = new HttpRequest(
@@ -47,12 +50,12 @@ class FinalizePaymentController
 
                                 $requestSent = $curl->get(
                                         '',
+                                        $tokenType,
                                         [],
                                         [
                                                 "Content-Type: application/json",
                                                 "Authorization: $tokenType $tokenName"
-                                        ],
-                                        $tokenType
+                                        ]   
                                 );
 
                                 /** remember to save links in database */
@@ -72,24 +75,20 @@ class FinalizePaymentController
                                 $completeUrl = $link['order_link'];
                                 //send post request to this url intent CAPTURE / POST request
 
-                                $checkToken = new CheckTokenExpiration;
-                                $results = $checkToken->check();
-
-                                $tokenName = $results['tokenName'];
-                                $tokenType = $results['tokenType'];
+                                [$tokenName, $tokenType] = $this->tokenValidatorRepository->get_token_data();
 
                                 $curl = new HttpRequest($completeUrl);
 
                                 $completePaymentUrl = $curl->post(
                                         '',
+                                        $tokenType,
                                         [
                                                 "intent" => "CAPTURE",
                                         ],
                                         [
                                                 "Content-Type: application/json",
                                                 "Authorization: $tokenType $tokenName"
-                                        ],
-                                        $tokenType
+                                        ]
                                 );
 
                                 $orderId = $completePaymentUrl['id'];
